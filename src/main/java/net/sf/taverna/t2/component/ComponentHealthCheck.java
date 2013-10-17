50b6fafa9f040bb1f8c9ec563220b49f50c44645
donal.k.fellows@man.ac.uk <donal.k.fellows@man.ac.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2013-10-17 12:48:30 +0000

    Reduce number/scope of warning suppressions. Consolidate node-list manipulation into one place, which in turn is implemented correctly.
    git-svn-id: http://taverna.googlecode.com/svn/unsorted/component/branches/component-1.1.0/component-activity@16178 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/component/ComponentHealthCheck.java
+++ src/main/java/net/sf/taverna/t2/component/ComponentHealthCheck.java
@@ -9,9 +9,8 @@ public class ComponentHealthCheck extends VisitKind {
 	public static final int NON_SHAREABLE = 20;
 	public static final int FAILS_PROFILE = 30;
 
-	@SuppressWarnings("rawtypes")
 	@Override
-	public Class<? extends Visitor> getVisitorClass() {
+	public Class<? extends Visitor<?>> getVisitorClass() {
 		return ComponentActivityUpgradeChecker.class;
 	}
 
