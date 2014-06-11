2262b22f157dfe1517526e8e209ce2aca3197f69
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2014-06-11 11:30:21 +0100

    Successful extraction of title and description from workflow bundle.

--- src/main/java/net/sf/taverna/t2/component/utils/AnnotationUtils.java
+++ src/main/java/net/sf/taverna/t2/component/utils/AnnotationUtils.java
@@ -9,9 +9,11 @@ import uk.org.taverna.scufl2.api.common.Child;
 import uk.org.taverna.scufl2.api.common.Scufl2Tools;
 import uk.org.taverna.scufl2.api.common.URITools;
 import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.ucfpackage.UCFPackage.ResourceEntry;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Statement;
 
 public class AnnotationUtils {
@@ -20,20 +22,34 @@ public class AnnotationUtils {
 	private Scufl2Tools tools = new Scufl2Tools();
 	private URITools uris = new URITools();
 
-	private WeakHashMap<Child<?>, Model> cache = new WeakHashMap<>();
 	public Model getAnnotationModel(Child<WorkflowBundle> subject) throws IOException {
+		return ModelFactory.createDefaultModel().add(getModel(subject));
+	}
+
+	private WeakHashMap<Child<?>, Model> cache = new WeakHashMap<>();
+
+	private static void readParse(Model model, WorkflowBundle bundle, String path)
+			throws IOException {
+		model.read(
+				new StringReader(bundle.getResources()
+						.getResourceAsString(path)), bundle.getGlobalBaseURI()
+						.resolve(path).toString(), "TTL");
+	}
+
+	public Model getModel(Child<WorkflowBundle> subject) throws IOException {
 		WorkflowBundle bundle = subject.getParent();
 		Model m = cache.get(subject);
 		if (m == null) {
 			m = ModelFactory.createDefaultModel();
+			long initialSize = m.size();
 			for (Annotation a : tools.annotationsFor(subject,
-					subject.getParent())) {
-				if (a.getBody().isAbsolute())
-					continue;
-				String rdf = bundle.getResources().getResourceAsString(
-						a.getBody().getPath());
-				m.read(new StringReader(rdf), a.getBody().toString());
-			}
+					subject.getParent()))
+				if (!a.getBody().isAbsolute())
+					readParse(m, bundle, a.getBody().getPath());
+			if (m.size() == initialSize)
+				for (ResourceEntry o : bundle.getResources()
+						.listResources("annotation").values())
+					readParse(m, bundle, o.getPath());
 			cache.put(subject, m);
 		}
 		return m;
@@ -41,11 +57,13 @@ public class AnnotationUtils {
 
 	public Statement getAnnotation(Child<WorkflowBundle> subject,
 			String uriForAnnotation) throws IOException {
-		Model m = getAnnotationModel(subject);
+		Model m = getModel(subject);
+		Property p = m.getProperty(uriForAnnotation);
 		return m.getResource(uris.uriForBean(subject).toString()).getProperty(
-				m.getProperty(uriForAnnotation));
+				p);
 	}
 
+	/** Get the title of the main workflow in a workflow bundle. */
 	public String getTitle(WorkflowBundle bundle, String defaultTitle) {
 		try {
 			Statement s = getAnnotation(bundle.getMainWorkflow(),
@@ -58,6 +76,7 @@ public class AnnotationUtils {
 		return defaultTitle;
 	}
 
+	/** Get the description of the main workflow in a workflow bundle. */
 	public String getDescription(WorkflowBundle bundle, String defaultDescription) {
 		try {
 			Statement s = getAnnotation(bundle.getMainWorkflow(),
