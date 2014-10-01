4318b29d1c964f4e554ea646e95841f3bc63e983
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2014-10-01 13:39:30 +0100

    Put the property names in a shared location.

---
+++ src/main/java/net/sf/taverna/t2/component/api/config/ComponentPropertyNames.java
@@ -0,0 +1,14 @@
+package net.sf.taverna.t2.component.api.config;
+
+/**
+ * The names of properties used in JSON configurations associated with
+ * components.
+ * 
+ * @author Donal Fellows
+ */
+public interface ComponentPropertyNames {
+	String REGISTRY_BASE = "registryBase";
+	String FAMILY_NAME = "familyName";
+	String COMPONENT_NAME = "componentName";
+	String COMPONENT_VERSION = "componentVersion";
+}
