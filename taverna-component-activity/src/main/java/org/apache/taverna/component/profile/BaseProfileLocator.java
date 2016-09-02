28d89a6e03acb87e1086b95207d1079327b1ba2a
Stian Soiland-Reyes <stain@apache.org>
2016-09-02 15:03:06 +0100

    Added Apache License header
    Covered by software grant from University of Manchester to ASF
    
    (removed a couple of double headers)

--- taverna-component-activity/src/main/java/org/apache/taverna/component/profile/BaseProfileLocator.java
+++ taverna-component-activity/src/main/java/org/apache/taverna/component/profile/BaseProfileLocator.java
@@ -1,7 +1,22 @@
-/**
- * 
- */
 package org.apache.taverna.component.profile;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 import static java.util.Locale.UK;
 import static org.apache.commons.httpclient.HttpStatus.SC_OK;
@@ -27,10 +42,6 @@ import org.apache.log4j.Logger;
 import org.apache.taverna.component.api.ComponentException;
 import org.apache.taverna.configuration.app.ApplicationConfiguration;
 
-/**
- * @author alanrw
- * 
- */
 public class BaseProfileLocator {
 	private static final String BASE_PROFILE_PATH = "BaseProfile.xml";
 	private static final String BASE_PROFILE_URI = "http://build.mygrid.org.uk/taverna/BaseProfile.xml";
